3ff923673a67efae3fcddadd46c7c32924ef12b3
HPNeo <hpneo@hotmail.com>
2015-02-20 00:10:03 -0600

    Show notification instead an alert window

---
+++ app/assets/javascripts/call_requests.js
@@ -0,0 +1,10 @@
+function showNotification(text, id) {
+  var notification = $('#new_call_request');
+  notification.find('p').text(text)
+  notification.find('a').attr('href', '/call_request/' + id + '/respond');
+  notification.addClass('visible');
+
+  setTimeout(function() {
+    // notification.removeClass('visible');
+  }, 2000);
+}
