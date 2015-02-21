4eae1ca6b4e93006dd5f50921ab169b0bf6eabd0
HPNeo <hpneo@hotmail.com>
2015-02-20 20:08:32 -0600

    fix

--- app/assets/javascripts/call_requests.js
+++ app/assets/javascripts/call_requests.js
@@ -1,7 +1,7 @@
 function showNotification(text, id) {
   var notification = $('#new_call_request');
   notification.find('p').text(text)
-  notification.find('a').attr('href', '/call_request/' + id + '/respond');
+  notification.find('a').attr('href', '/call_requests/' + id + '/respond');
   notification.addClass('visible');
 
   setTimeout(function() {
